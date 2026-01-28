7ecb217a099b01223eeb356daa5e1c83134bdabc
tomasgebauer00 <tomasgebauer00@gmail.com>
2026-01-28 19:24:28 +0100

    logo fix

---
+++ src/app/icon.tsx
@@ -0,0 +1,50 @@
+import { ImageResponse } from 'next/og';
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 20,
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '50%', // Tohle dělá ten dokonalý kruh
+        }}
+      >
+        {/* SVG ikona letadla */}
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          style={{ transform: 'rotate(-45deg)', marginBottom: '2px', marginLeft: '2px' }} // Natočení letadla nahoru
+        >
+          <path d="M22 2L11 13" />
+          <path d="M22 2L15 22L11 13L2 9L22 2Z" />
+        </svg>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  );
+}
